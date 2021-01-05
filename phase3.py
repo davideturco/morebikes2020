8baef17c0406897d55602962924c8e4d18dcc3aa
davideturco <davide.turco@bristol.ac.uk>
2021-01-05 23:24:17 +0000

    All the six modules now included for phase3

--- phase3.py
+++ phase3.py
@@ -5,6 +5,15 @@ from sklearn.linear_model import Ridge
 from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import train_test_split
 
+MODELS = {'short': 'short_averaged.csv',
+          'short_temp': 'short_temp_averaged.csv',
+          'full': 'full_averaged.csv',
+          'full_temp': 'full_temp_averaged.csv',
+          'short_full': 'short_full_averaged.csv',
+          'short_full_temp': 'short_full_temp_averaged.csv'
+
+          }
+
 
 def prepare_general_dataset():
     df = load_general('Train/Train')
@@ -18,8 +27,8 @@ def prepare_general_dataset():
     return df
 
 
-def prepare_model():
-    model = pd.read_csv('full_temp_averaged.csv')
+def prepare_model(model_path):
+    model = pd.read_csv(model_path)
     features = model['feature']
 
     # weights
@@ -32,16 +41,15 @@ def prepare_model():
     return intercept, weights, features_used
 
 
-def fit_ridge(X_train, Y_train):
+def fit_ridge(train_x, train_y):
     model = Ridge()
-    model.fit(X_train, Y_train)
+    model.fit(train_x, train_y)
     return model.intercept_, model.coef_
 
 
 def average_coefficients(intercept_ridge, coefficients_ridge, intercept_pretrained, coefficients_pretrained):
-    # TODO: check that this actually returns averaged coefficients!!
     averaged_intercept = (intercept_ridge + 12 * intercept_pretrained) / 13
-    averaged_coefficients = (coefficients_ridge.add(12 * coefficients_ridge)) / 13
+    averaged_coefficients = (coefficients_ridge.add(12 * coefficients_pretrained)) / 13
     return averaged_intercept, averaged_coefficients
 
 
@@ -51,20 +59,25 @@ def integrated_model(X, intercept, coefficients):
 
 
 def main():
+    # TODO: maybe try to tune Ridge()
     df = prepare_general_dataset()
-    intercept_ridge, weights, features_used = prepare_model()
     df_train, df_test = train_test_split(df)
-    X_train = df_train.loc[:, df_train.columns != 'bikes']
-    X_train = X_train.filter(items=features_used)
-    Y_train = df_train['bikes']
-    intercept, coefficients = fit_ridge(X_train, Y_train)
-    coefficients = pd.Series(coefficients, index=features_used)
-    q, a = average_coefficients(intercept_ridge, weights, intercept, coefficients)
-    predictions = integrated_model(X_train, q, a)
-    mae = mean_absolute_error(predictions, Y_train)
-    print(mae)
-    # print(intercept)
-    # print(coefficients)
+
+    for key, value in MODELS.items():
+        intercept_ridge, weights, features_used = prepare_model(value)
+        X_train = df_train.loc[:, df_train.columns != 'bikes']
+        X_train = X_train.filter(items=features_used)
+        Y_train = df_train['bikes']
+        X_test = df_test.loc[:, df_test.columns != 'bikes']
+        X_test = X_test.filter(items=features_used)
+        Y_test = df_test['bikes']
+
+        intercept, coefficients = fit_ridge(X_train, Y_train)
+        coefficients = pd.Series(coefficients, index=features_used)
+        q, a = average_coefficients(intercept_ridge, weights, intercept, coefficients)
+        predictions = integrated_model(X_test, q, a)
+        mae = mean_absolute_error(predictions, Y_test)
+        print(f"\nThe MAE for model {key} is {mae}\n")
 
 
 if __name__ == '__main__':
